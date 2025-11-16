1bef86fa3ab90111cee31e0fd8a6f1f6399c53a0
Karan Singh <veerkaransinghmgm@gmail.com>
2025-11-16 17:48:02 -0500

    Initial Commit _ Hands on practice on Loose coupling!

---
+++ src/main/java/com/loose/coupling/NewDatabaseProvider.java
@@ -0,0 +1,9 @@
+package com.loose.coupling;
+
+public class NewDatabaseProvider implements UserDataProvider{
+    @Override
+    public String getUserDetails() {
+        return "New Database in Action";
+    }
+
+}
