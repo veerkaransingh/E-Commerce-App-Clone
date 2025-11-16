60e1055491e0f88b2cf4e30bdf5287d1cc4e2ea0
Karan Singh <veerkaransinghmgm@gmail.com>
2025-11-16 17:49:03 -0500

    Initial Commit _ Hands on practice on Tight coupling!

--- src/main/java/com/tight/coupling/UserDatabase.java
+++ src/main/java/com/tight/coupling/UserDatabase.java
@@ -1,5 +1,9 @@
 package com.tight.coupling;
 
 public class UserDatabase {
+    public String getUserDetails(){
+        //Directly access database here
+        return "User details from the Database";
+    }
 
 }
