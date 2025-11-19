3a9765f7c14159bcc84e8c28bc64bb25058a8c1b
Karan Singh <veerkaransinghmgm@gmail.com>
2025-11-18 22:03:11 -0500

    Initial Commit - Setting up bean

--- src/main/java/car/example/bean/App.java
+++ src/main/java/car/example/bean/App.java
@@ -1,7 +1,13 @@
 package car.example.bean;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
-
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+        MyBean mybean = (MyBean) context.getBean("myBean");
+        System.out.println(mybean);
     }
 }
