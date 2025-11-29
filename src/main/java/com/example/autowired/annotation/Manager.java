373fa23f79cc118f4ce0b40eac6848a445c76e19
Karan Singh <veerkaransinghmgm@gmail.com>
2025-11-28 20:18:25 -0500

    Initial Commit

--- src/main/java/com/example/autowired/annotation/Manager.java
+++ src/main/java/com/example/autowired/annotation/Manager.java
@@ -1,17 +1,20 @@
 package com.example.autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Manager {
 
+    @Autowired  // field injection
+    @Qualifier("employee") // if there are two beans registered with the same name, @qualifier is used
     private Employee employee;
 
-    @Autowired
-    public Manager(Employee employee) {
-        this.employee = employee;
-    }
+   // @Autowired // this is doing constructor injection
+    //public Manager(Employee employee) {
+      //  this.employee = employee;
+    //}
 
     @Override
     public String toString() {
