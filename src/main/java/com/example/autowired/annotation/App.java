04ea89ac61d4201a3c0ab04dd530c735416fd420
Karan Singh <veerkaransinghmgm@gmail.com>
2025-12-14 21:41:02 -0500

    Initial Commit

--- src/main/java/com/example/autowired/annotation/App.java
+++ src/main/java/com/example/autowired/annotation/App.java
@@ -2,7 +2,7 @@ package com.example.autowired.annotation;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+//
 public class App {
     public static void main(String[] args) {
 
